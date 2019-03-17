f8beda12d6e2a55429166a3e5b19aef52cdb09df
Andrew Smith <smith.andrew.l@wwinc.com>
2019-03-17 15:31:44 -0500

    Added boilerplate canvas code.

---
+++ mandelbrot.js
@@ -0,0 +1,10 @@
+
+
+
+function render() {
+    const canvas = document.getElementById('can');
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10, 10, 150, 100);
+}
