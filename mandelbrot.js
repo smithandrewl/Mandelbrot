667517d92e6e4f4450c8a535d7e024944f8e6310
Andrew Smith <smith.andrew.l@wwinc.com>
2019-03-17 15:44:15 -0500

    Test pattern is drawn to the canvas.

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,10 +1,32 @@
 
 
 
-function render() {
-    const canvas = document.getElementById('can');
-    const ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 150, 100);
+
+window.onload = function() {
+    console.log("setup");
+    window.canvas = document.getElementById('can');
+    window.ctx = canvas.getContext('2d');
+    window.id = ctx.createImageData(1,1); // only do this once per page
+    window.d  = id.data;                        // only do this once per page
+
+}
+
+function render() {
+
+
+    for(var i = 0; i < 800; i++) {
+        for(var j =0; j < 600; j++){
+            setPixel(i, j, i % 255,j %255,i % 255);
+        }
+    }
+}
+
+
+function setPixel(x,y, r,g,b) {
+    window.d[0]   = r;
+    window.d[1]   = g;
+    window.d[2]   = b;
+    window.d[3]   = 255;
+    window.ctx.putImageData( window.id, x, y );
 }
